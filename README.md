f8ecef289a6092fab5ab24ef69c8b610a414485f
Roman Kudravcev <roman.kudravcev@stud.uni-bamberg.de>
2023-07-26 21:42:43 +0200

    add structure and schema validation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# jsonUebung
+# JSON Exercise
+This is a possible exercise that covers the topic of JSON. The exercise includes the following aspects:
+
+- Transformation of objects into a JSON file
+- Transformation of a JSON file into objects
+- Validation of JSON files based on a schema
+- Creation of a schema using a tool
+
+## Schema Validation
+Baeldung provides an example with a suitable library in their [Introduction to JSON Schema](https://www.baeldung.com/introduction-to-json-schema-in-java). This example has been revised for proper integration into applications, and you can find it in the `JSONValidation` class.
---
+++ src/main/java/jsonfun/JSONValidation.java
@@ -0,0 +1,53 @@
+package jsonfun;
+
+import com.networknt.schema.*;
+import com.fasterxml.jackson.databind.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+
+public class JSONValidation {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V4);
+
+        try {
+            // Load the JSON schema
+            InputStream schemaInputStream = JSONValidation.class.getResourceAsStream("/schema.json");
+            if (schemaInputStream == null) {
+                throw new IllegalStateException("Schema file not found in resources.");
+            }
+            JsonSchema jsonSchema = factory.getSchema(schemaInputStream);
+
+            // Load the JSON data to validate
+            InputStream jsonInputStream = JSONValidation.class.getResourceAsStream("/film.json");
+            if (jsonInputStream == null) {
+                throw new IllegalStateException("JSON data file not found in resources.");
+            }
+            JsonNode jsonNode = mapper.readTree(jsonInputStream);
+
+            // Validate the JSON data against the schema
+            Set<ValidationMessage> errors = jsonSchema.validate(jsonNode);
+
+            // If there are validation errors, throw an exception
+            if (!errors.isEmpty()) {
+                StringBuilder errorMessages = new StringBuilder();
+                for (ValidationMessage error : errors) {
+                    errorMessages.append(error.getMessage()).append("\n");
+                }
+                throw new IllegalArgumentException("JSON data is invalid:\n" + errorMessages);
+            }
+
+                // JSON data is valid, continue with your logic here
+                System.out.println("The validation was successful!");
+
+        } catch (IOException e) {
+            // Handle any I/O errors
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            // Handle the exception for invalid JSON data
+            System.err.println(e.getMessage());
+        }
+    }
+}
